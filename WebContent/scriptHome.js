8d2cde326084794a134596857d57781ac624b05c
AxelGalliot <axelgalliot@live.fr>
2016-11-29 13:57:35 -0500

    maj again

--- WebContent/scriptHome.js
+++ WebContent/scriptHome.js
@@ -1,12 +1,14 @@
 function loadMatchList(){
 	$.get('GameList', function(listeMatch){
 		$.each(listeMatch, function(index,value){
-			console.log(value);
+			$.each(value, function(index2,value2){
+				ajouterLigne(value2.equipeA, value2.equipeB, value2.temps);
+			});
 		});
 	});
 }
 
-function ajouterLigne(match){
+function ajouterLigne(equipeA, equipeB, temps){
     var tableau = document.getElementById("tableResult");
     var ligne = tableau.insertRow(-1);
 	ligne.onclick = (function(){
@@ -19,7 +21,9 @@ function ajouterLigne(match){
 		ligne.style.backgroundColor = "#ffffff";
 	});
     var colonne1 = ligne.insertCell(0);
-    colonne1.innerHTML = "test1";
+    colonne1.innerHTML = equipeA;
     var colonne2 = ligne.insertCell(1);
-    colonne2.innerHTML = "test2";
+    colonne2.innerHTML = temps;
+    var colonne3 = ligne.insertCell(2);
+    colonne3.innerHTML = equipeB;
 }
