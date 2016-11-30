728fe8c97c17acff3090ee713c56d8d4d75a83b5
AxelGalliot <axelgalliot@live.fr>
2016-11-30 10:34:05 -0500

    Changement CSS + MaJ Javascript pour affichage

--- WebContent/scriptHome.js
+++ WebContent/scriptHome.js
@@ -1,32 +1,49 @@
+var ds_ListeDesMatchs;
+var flag_Split = false;
+
 function loadMatchList(){
 	clearTable();
 	$.get('GameList', function(listeMatch){
+		ds_ListeDesMatchs = listeMatch;
 		$.each(listeMatch, function(index,value){
 			$.each(value, function(index2,value2){
-				ajouterLigne(value2.equipeA, value2.equipeB, value2.temps, value2.id);
+				ajouterLigne(value2.equipeA, value2.equipeB, value2.temps);
 			});
 		});
 	});
 }
 
 function clearTable(){
-	var table = document.getElementById("tableResult");
+	var table = document.getElementById("tableMatch");
 	while(table.rows.length >0){
 		table.deleteRow(0);
 	}
 }
 
-function ajouterLigne(equipeA, equipeB, temps, id){
-    var tableau = document.getElementById("tableResult");
+function ajouterLigne(equipeA, equipeB, temps){
+	var tableau = document.getElementById("tableMatch");
     var ligne = tableau.insertRow(-1);
 	ligne.onclick = (function(){
-		alert("test");
+		if(flag_Split == false){
+			$('.container').toggleClass('expanded');
+			flag_Split = true;
+		}
 	});
 	ligne.onmouseover = (function(){
-		ligne.style.backgroundColor = "#9999ff";
+		if(flag_Split == false){
+			ligne.style.backgroundColor = "#9999ff";
+		}
+		else{
+			ligne.style.backgroundColor = "#9999ff";
+		}
 	});
 	ligne.onmouseout = (function(){
-		ligne.style.backgroundColor = "#ffffff";
+		if(flag_Split == false){
+			ligne.style.backgroundColor = "#899ED3";
+		}
+		else{
+			ligne.style.backgroundColor = "#C3CBDD";
+		}
 	});
     var colonne1 = ligne.insertCell(0);
     colonne1.innerHTML = equipeA;
@@ -34,6 +51,5 @@ function ajouterLigne(equipeA, equipeB, temps, id){
     colonne2.innerHTML = temps;
     var colonne3 = ligne.insertCell(2);
     colonne3.innerHTML = equipeB;
-    var colonne4 = ligne.insertCell(2);
-    colonne4.innerHTML = id;
 }
+
