8fd976e34e3894760724bce7bfea253a7bb08b2e
WillLnn <william_lanon@msn.com>
2016-11-30 14:42:12 -0500

    Revert "Details des matchs"
    This reverts commit 164bb85ed5909509bbdc80ea7894eb61c20af909.

--- WebContent/Home.jsp
+++ WebContent/Home.jsp
@@ -6,6 +6,7 @@
 		<title>HockeyLive</title>
 		<script src="http://code.jquery.com/jquery-latest.js"></script>
 		<script src="scriptHome.js"></script>
+		<!-- <link rel="stylesheet" href="styles.css" /> -->
 		<link rel="stylesheet" type="text/css" href="home.css" />
 	</head>
 
@@ -14,6 +15,7 @@
     	<div class="left">
 			<button id="btRefresh" type="button" onclick="loadMatchList()">Refresh</button>
 			<h1>LISTE DES MATCHS EN COURS</h1>
+			<!-- <img src="img/bandeau.png"> -->
 			<div class="table">
 				<table id="tableMatch" class="tableMatch" ></table>
 			</div>
--- WebContent/scriptHome.js
+++ WebContent/scriptHome.js
@@ -2,32 +2,25 @@ var ds_ListeDesMatchs;
 var flag_Split = false;
 
 function loadMatchList(){
-	clearTable("tableMatch");
+	clearTable();
 	$.get('GameList', function(listeMatch){
 		ds_ListeDesMatchs = listeMatch;
 		$.each(listeMatch, function(index,value){
 			$.each(value, function(index2,value2){
-				ajouterLigne(value2.id, value2.equipeA, value2.equipeB, value2.temps);
+				ajouterLigne(value2.equipeA, value2.equipeB, value2.temps);
 			});
 		});
 	});
 }
 
-function loadMatchDetail(id){
-	clearTable("tableMatchDetail");
-	$.get('GameDetail', {id: id}, function(matchDetail){
-				console.log(matchDetail.temps);
-	});
-}
-
-function clearTable(table){
-	var table = document.getElementById(table);
+function clearTable(){
+	var table = document.getElementById("tableMatch");
 	while(table.rows.length >0){
 		table.deleteRow(0);
 	}
 }
 
-function ajouterLigne(id, equipeA, equipeB, temps){
+function ajouterLigne(equipeA, equipeB, temps){
 	var tableau = document.getElementById("tableMatch");
     var ligne = tableau.insertRow(-1);
 	ligne.onclick = (function(){
@@ -35,7 +28,6 @@ function ajouterLigne(id, equipeA, equipeB, temps){
 			$('.container').toggleClass('expanded');
 			flag_Split = true;
 		}
-		loadMatchDetail(id);
 	});
 	ligne.onmouseover = (function(){
 		if(flag_Split == false){
@@ -54,13 +46,10 @@ function ajouterLigne(id, equipeA, equipeB, temps){
 		}
 	});
     var colonne1 = ligne.insertCell(0);
-    colonne1.innerHTML = id;
+    colonne1.innerHTML = equipeA;
     var colonne2 = ligne.insertCell(1);
-    colonne2.innerHTML = equipeA;
+    colonne2.innerHTML = temps;
     var colonne3 = ligne.insertCell(2);
-    colonne3.innerHTML = temps;
-    var colonne4 = ligne.insertCell(3);
-    colonne4.innerHTML = equipeB;
+    colonne3.innerHTML = equipeB;
 }
-
 
